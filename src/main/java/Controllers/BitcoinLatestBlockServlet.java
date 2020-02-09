e70d7a252ee40be79f048020ee350c4ae2dacfa0
imerghichi <hypathie14@gmail.com>
2020-02-09 23:52:46 +0000

    final?

--- src/main/java/Controllers/BitcoinLatestBlockServlet.java
+++ src/main/java/Controllers/BitcoinLatestBlockServlet.java
@@ -1,5 +1,7 @@
 package Controllers;
 
+import Model.BitcoinStatsService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,13 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class BitcoinLatestBlockServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
-    }
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        BitcoinStatsService bitcoinStatsService =new BitcoinStatsService();
+        req.setAttribute("total fees btc", bitcoinStatsService.get_total_fees_btc());
+        req.setAttribute("total bc",bitcoinStatsService.get_totalbc());
+        req.setAttribute("miners revenue btc",bitcoinStatsService.getminers_revenue_btc());
+        req.setAttribute("minutes between blocks", bitcoinStatsService.getminutes_btween_blocks());
+        req.setAttribute("block size", bitcoinStatsService.get_block_size());
+        req.setAttribute("n blocks total", bitcoinStatsService.getn_blocks_total());
+        req.setAttribute("difficulty", bitcoinStatsService.getdiffuculty());
     }
 }
